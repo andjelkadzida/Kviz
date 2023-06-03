64a59d8b63aeabbb14d9dc7cbb8a69fc70301186
Andjelka Dzida <andjelka.dzida@hpe.com>
2023-06-03 16:34:25 +0200

    Added message for problem reporting.

--- app/src/main/java/andjelka/kvizpliz/Report_Problem.java
+++ app/src/main/java/andjelka/kvizpliz/Report_Problem.java
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
 
 public class Report_Problem extends AppCompatActivity {
 
@@ -56,6 +62,24 @@ public class Report_Problem extends AppCompatActivity {
                 }
                 try {
                     startActivity(Intent.createChooser(intent, getResources().getString(R.string.send)));
+                    boolean activityStarted = isActivityStarted(intent);
+
+                    if(activityStarted) {
+                        Handler handler = new Handler();
+                        handler.postDelayed(() -> {
+                            //Poruka o poslatoj prijavi
+                            Snackbar snackbar = Snackbar.make(view, getResources().getString(R.string.msgSent), Snackbar.LENGTH_INDEFINITE);
+                            TextView messageView = snackbar.getView().findViewById(R.id.snackbar_text);
+                            messageView.setMaxLines(5);
+                            snackbar.setAction(getResources().getString(R.string.OK), v12 -> snackbar.dismiss());
+                            snackbar.setActionTextColor(getResources().getColor(android.R.color.holo_green_dark)).show();
+                        }, 5000);
+
+                        //Obrisi polja
+                        contact.getText().clear();
+                        problemDescription.getText().clear();
+                        radioGroup.clearCheck();
+                    }
                 }
                 catch (Exception ex)
                 {
@@ -74,4 +98,10 @@ public class Report_Problem extends AppCompatActivity {
                 }
             });
     }
+    private boolean isActivityStarted(Intent intent) {
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(intent, 0);
+        return !activities.isEmpty();
+    }
+
 }
